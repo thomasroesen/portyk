b6cc65d72fefbf152f1ba2a893a4b4b3e57e119d
Thomas Roesen <thomroe@hotmail.com>
2017-02-06 08:28:49 +0100

    add routing add navigation

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,64 @@
 <template>
 <div id="app">
-  <el-menu theme="dark" default-active="1" class="navigation" mode="horizontal">
-    <router-link v-for="(route, index) in routes" :to="route.path">
-      <el-menu-item :index="index.toString()">{{route.name}}</el-menu-item>
-    </router-link>
-    <router-view></router-view>
+  <el-row type="flex" justify="center" align="middle">
+    <el-col :span=8><img class="logo" src="./assets/logo.png" alt=""></el-col>
+    <el-col :span=16>
+      <el-menu :default-active="routes[0].name" class="nav" mode="horizontal" router>
+          <el-menu-item class="nav-item" v-for="route in routes" :index="route.name" :route="route">{{route.name}}</el-menu-item>
+        </el-menu>
+    </el-col>
+  </el-row>
+  <el-row>
+      <router-view></router-view>
+  </el-row>
 </div>
 </template>
 
 <script>
-import routes from './router-config'
-export default {
-  name: 'app',
-  data () {
-    return {
-      routes
-    }
-  },
-  methods: {}
-}
+  import routes from './router-config'
+  export default {
+    name: 'app',
+    data () {
+      return {
+        routes
+      }
+    },
+    methods: {}
+  }
 </script>
 
-<style>
-#app {
-  font-family: "Helvetica Neue", Helvetica, "PingFang SC", "Hiragino Sans GB", "Microsoft YaHei", "微软雅黑", Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
-}
+<style lang="scss">
+  @import url(http://fonts.googleapis.com/css?family=Roboto);
+  #app {
+    font-family: 'Roboto', sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    color: #2c3e50;
+  }
 
-.navigation {
-  background: none;
-}
-.el-menu-item:hover {
-  background-color: none !important;
-}
+  .nav {
+    background: none;
+  }
+
+  .el-menu-item.nav-item {
+    font-size: 1.1em;
+    font-weight: 100;
+    border-bottom: none;
+    margin-right: 20px;
+  }
+
+  .el-menu-item.nav-item:hover {
+    background-color: transparent;
+    border-bottom: 3px solid #20a0ff;
+  }
+
+  .el-menu-item.is-active {
+    border-bottom: 3px solid #20a0ff;
+  }
+
+  .logo {
+    max-width: 50px;
+    display: flex;
+    margin: 0 auto;
+  }
 </style>
